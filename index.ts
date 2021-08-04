c7b1c2e5a09375946fb6abfa81dd4eb24caccc54
horiguchi <horiguchi@sannoweb.info>
2021-08-05 00:24:05 +0900

    tweetの読み取り

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 
-import Twitter from "twitter"
+var Twitter = require('twitter');
 import dotenv from "dotenv";
+
+dotenv.config();
 
 const client = new Twitter({
     consumer_key: process.env.CUSTOMER_KEY,
@@ -9,3 +11,12 @@ const client = new Twitter({
     access_token_secret: process.env.TOKEN_SECRETS
 });
 
+const params = {screen_name: 'nodejs',count:2};
+
+client.get('statuses/home_timeline', params, function(error: any, tweets: any, response: any) {
+    if (!error) {
+      console.log(tweets)
+    } else {
+      console.log('hoge');
+    }
+})
